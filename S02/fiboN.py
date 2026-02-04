c6abb53df3c0cdfa309bc369623d461cf6b69172
jbaena <j.baena.2025@alumnos.urjc.es>
2026-02-04 17:41:42 +0100

    first exercise done

---
+++ S02/fiboN.py
@@ -0,0 +1,18 @@
+
+def fibon():
+    a = 0
+    b = 1
+    i = 2
+    while i < 15:
+        c = a + b
+        a = b
+        b = c
+        i += 1
+        if i == 5:
+            print("The 5th element of the fibonacci series is:", c)
+        elif i == 10:
+            print("The 10th element of the fibonacci series is:", c)
+        elif i == 15:
+            print("The 15th element of the fibonacci series is:", c)
+fibon()
+
