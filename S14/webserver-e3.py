8e0da9d6eadeda612d8301cacba3d69aba5ccb4e
jbaena <j.baena.2025@alumnos.urjc.es>
2026-04-07 16:21:33 +0200

    saved S14

---
+++ S14/webserver-e3.py
@@ -0,0 +1,46 @@
+import http.server
+import socketserver
+import termcolor
+from pathlib import Path
+
+PORT = 8080
+
+socketserver.TCPServer.allow_reuse_address = True
+
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        termcolor.cprint(self.requestline, 'green')
+
+        if self.path == "/" or self.path == "/index.html":
+            contents = Path("index.html").read_text()
+        elif self.path == "/blue.html":
+            contents = Path("blue.html").read_text()
+        elif self.path == "/green.html":
+            contents = Path("green.html").read_text()
+        elif self.path == "/pink.html":
+            contents = Path("pink.html").read_text()
+        else:
+            contents = Path("error.html").read_text()
+
+        self.send_response(200)
+
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(contents.encode()))
+        self.end_headers()
+
+        self.wfile.write(contents.encode())
+
+        return
+
+
+Handler = TestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at PORT {PORT}")
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("\nStopped by the user")
+        httpd.server_close()
