1cd3fdb291f9358c01c998ae9caf5cfbe644a4aa
jbaena <j.baena.2025@alumnos.urjc.es>
2026-05-05 16:50:08 +0200

    finished

--- Final-project/server.py
+++ Final-project/server.py
@@ -178,6 +178,142 @@ class GenomeHandler(http.server.BaseHTTPRequestHandler):
                 except Exception as e:
                     self.send_response(500)
                     contents = f"<h1>Internal Error: {e}</h1>"
+
+        elif path == "/geneSeq":
+            params = parse_qs(parsed_url.query)
+            gene_name = params.get('gene', [''])[0]
+
+            if not gene_name:
+                self.send_response(404)
+                contents = Path('html/error.html').read_text()
+            else:
+                url = f"https://rest.ensembl.org/lookup/symbol/homo_sapiens/{gene_name}"
+
+                try:
+                    response = requests.get(url, headers={"Content-Type": "application/json"})
+
+                    if response.status_code == 200:
+                        data = response.json()
+                        ensembl_id = data.get('id')
+
+                        url2 = f"https://rest.ensembl.org/sequence/id/{ensembl_id}"
+                        response2 = requests.get(url2, headers={"Content-Type": "application/json"})
+                        data2 = response2.json()
+                        seq = data2.get("seq")
+                        template = read_html_file("geneseq.html")
+                        contents = template.render(info={
+                            "gene": gene_name,
+                            "seq": seq
+                        })
+                        self.send_response(200)
+
+                    else:
+                        self.send_response(404)
+                        contents = Path('html/error.html').read_text()
+
+                except Exception as e:
+                    self.send_response(500)
+                    contents = f"<h1>Internal Error: {e}</h1>"
+
+        elif path == "/geneInfo":
+            params = parse_qs(parsed_url.query)
+            gene_name = params.get('gene', [''])[0]
+
+            if not gene_name:
+                self.send_response(404)
+                contents = Path('html/error.html').read_text()
+            else:
+                url = f"https://rest.ensembl.org/lookup/symbol/homo_sapiens/{gene_name}"
+
+                try:
+                    response = requests.get(url, headers={"Content-Type": "application/json"})
+
+                    if response.status_code == 200:
+                        data = response.json()
+                        ensembl_id = data.get('id')
+
+                        url2 = f"https://rest.ensembl.org/lookup/id/{ensembl_id}"
+                        response2 = requests.get(url2, headers={"Content-Type": "application/json"})
+
+                        data2 = response2.json()
+                        start = data2.get("start")
+                        end = data2.get("end")
+                        identifier = data2.get("id")
+                        chr_name = data2.get("seq_region_name")
+                        length = int(end) - int(start)
+
+                        template = read_html_file("geneinfo.html")
+                        contents = template.render(info={
+                            "gene_name": gene_name,
+                            "chr_name": chr_name,
+                            "id": identifier,
+                            "start": start,
+                            "end": end,
+                            "length": length
+                        })
+                        self.send_response(200)
+
+                    else:
+                        self.send_response(404)
+                        contents = Path('html/error.html').read_text()
+
+                except Exception as e:
+                    self.send_response(500)
+                    contents = f"<h1>Internal Error: {e}</h1>"
+
+        elif path == "/geneCalc":
+            params = parse_qs(parsed_url.query)
+            gene_name = params.get('gene', [''])[0]
+
+            if not gene_name:
+                self.send_response(404)
+                contents = Path('html/error.html').read_text()
+            else:
+                url = f"https://rest.ensembl.org/lookup/symbol/homo_sapiens/{gene_name}"
+
+                try:
+                    response = requests.get(url, headers={"Content-Type": "application/json"})
+
+                    if response.status_code == 200:
+                        data = response.json()
+                        ensembl_id = data.get('id')
+
+                        url2 = f"https://rest.ensembl.org/sequence/id/{ensembl_id}"
+                        response2 = requests.get(url2, headers={"Content-Type": "application/json"})
+                        data2 = response2.json()
+                        seq = data2.get("seq")
+                        remove = seq.find("\n")
+                        clean_seq = seq[remove:]
+                        length = len(clean_seq)
+                        bases = {"A": 0, "T": 0, "C": 0, "G": 0}
+                        for t in clean_seq:
+                            if t in bases:
+                                bases[t] += 1
+
+                        adenine = round((bases["A"] / length) * 100, 2)
+                        thymine = round((bases["T"] / length) * 100, 2)
+                        cytosine = round((bases["C"] / length) * 100, 2)
+                        guanine = round((bases["G"] / length) * 100, 2)
+
+                        template = read_html_file("geneseq.html")
+                        contents = template.render(info={
+                            "gene": gene_name,
+                            "length": length,
+                            "adenine": adenine,
+                            "thymine": thymine,
+                            "cytosine": cytosine,
+                            "guanine": guanine
+                        })
+                        self.send_response(200)
+
+                    else:
+                        self.send_response(404)
+                        contents = Path('html/error.html').read_text()
+
+                except Exception as e:
+                    self.send_response(500)
+                    contents = f"<h1>Internal Error: {e}</h1>"
+
         else:
             try:
                 contents = Path('html/error.html').read_text()
